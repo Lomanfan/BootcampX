dfcf786ab3a57b3e5e1c75d5bf5baf3ae60a864e
Liuming <lfan.canada@gmail.com>
2021-03-30 04:22:40 +0000

    GROUP BY, HAVING, ORDER BY, AVG

--- 3_queries_group-by/3_queries_group-by.sql
+++ 3_queries_group-by/3_queries_group-by.sql
@@ -51,12 +51,12 @@ ORDER BY average_assignment_duration DESC;
 
 --Get the students who's average time it takes to complete an assignment is less than the average estimated time it takes to complete an assignment.
 SELECT students.name as student, 
-avg(assignment_submissions.duration) as average_assignment_duration, 
-avg(assignments.duration) as average_estimated_duration
+      AVG(assignment_submissions.duration) as average_assignment_duration, 
+      AVG(assignments.duration) as average_estimated_duration
 FROM students
 JOIN assignment_submissions ON student_id = students.id
 JOIN assignments ON assignments.id = assignment_id
 WHERE end_date IS NULL
 GROUP BY student
-HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
+HAVING AVG(assignment_submissions.duration) < avg(assignments.duration)
 ORDER BY average_assignment_duration;
